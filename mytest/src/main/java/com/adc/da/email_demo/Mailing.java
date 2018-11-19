831a194051afde3d64f7fdde210bbc47e29c13ce
yueBen <1154451750@qq.com>
2018-11-19 12:36:08 +0800

    邮件发送——yueben

--- mytest/src/main/java/com/adc/da/email_demo/Mailing.java
+++ mytest/src/main/java/com/adc/da/email_demo/Mailing.java
@@ -1,14 +1,13 @@
 package com.adc.da.email_demo;
 
+
 import com.adc.da.notify.service.NotifyService;
 import com.adc.da.util.http.ResponseMessage;
 import com.adc.da.util.http.Result;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/${restPath}/email")
@@ -16,22 +15,19 @@ public class Mailing {
 
 
     @PostMapping("/send")
-    public ResponseMessage emailSend(HttpServletRequest request, HttpServletResponse response){
-        String toEamil = request.getParameter("toEamil");
-        String emailTitle = request.getParameter("emailTitle");
-        String emailBody = request.getParameter("emailBody");
-        System.out.println(toEamil + "_" + emailTitle + "_" + emailBody);
-        if(toEamil != null){
+    public ResponseMessage emailSend(@RequestBody EmailEo  emailEo){
+
+        System.out.println(emailEo.getToEamil() + "_" + emailEo.getEmailTitle() + "_" + emailEo.getEmailBody());
+        if(emailEo.getToEamil() != null){
             try {
                 NotifyService notifyService = new NotifyService();
-                notifyService.sendEmail(toEamil,emailTitle,emailBody);
-                return Result.success("发送成功");
+                notifyService.sendEmail(emailEo.getToEamil(),emailEo.getEmailTitle(),emailEo.getEmailBody());
             }catch (Exception e){
                 e.printStackTrace();
-                Result.error("发送失败！");
+                return Result.error("发送失败!!!");
             }
         }
 
-        return Result.error("发送地址为空");
+        return Result.success("发送成功!!!");
     }
 }
