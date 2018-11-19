7d2dfca34998e1365e6d7f3b8b726aeec14cd3ff
yueBen <1154451750@qq.com>
2018-11-19 16:36:47 +0800

    邮件发送(带附件)——yueben

--- mytest/src/main/java/com/adc/da/email_demo/Mailing.java
+++ mytest/src/main/java/com/adc/da/email_demo/Mailing.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
