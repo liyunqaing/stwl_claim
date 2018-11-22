df3d317c2d443b64876f6c8b2c410a096e343854
yueBen <1154451750@qq.com>
2018-11-22 22:03:14 +0800

    redis操作Hash表和List列表--yueben

--- mytest/src/main/java/com/adc/da/quartz/config/Task.java
+++ mytest/src/main/java/com/adc/da/quartz/config/Task.java
@@ -3,6 +3,9 @@ package com.adc.da.quartz.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
 * @Description:   _单任务实现类
 * @Author:         yueben
@@ -13,6 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Task {
 
     public void task1(){
-        System.out.println("Task_1");
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(date));
     }
 }
