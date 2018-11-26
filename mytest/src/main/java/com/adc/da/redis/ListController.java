73e59424f96d42a862ebd05376eb3265b831f29e
yueBen <1154451750@qq.com>
2018-11-26 09:03:12 +0800

    线程——yueben

--- mytest/src/main/java/com/adc/da/redis/ListController.java
+++ mytest/src/main/java/com/adc/da/redis/ListController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.BinaryClient;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class ListController {
         Jedis j = JedisUtil.getInstance().getJedis();
 
         //将一个或多个值插入到列表头部(该方法可以插入一个不存在的List中）
-        Long lpush = j.lpush("mylist", "a", "b", "c", "d", "1", "2", "3", "4");
+        Long lpush = j.lpush("mylist", "a", "b", "c", "d", "1", "2", "3", "4");     //"4"是下标为0的元素
         System.out.println("lpush(key,values...)的返回值是列表长度：" + lpush);
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
@@ -43,14 +44,14 @@ public class ListController {
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
-        //将一个或多个值插入到列表头部（该方法不能插入一个不存在的List中）
-        Long lpushx1 = j.lpushx("mylist", "aa", "bb", "cc", "dd");
-        Long lpushx2 = j.lpushx("mylist1", "aa", "bb", "cc", "dd");
-        System.out.println("lpushx(key,values...)的返回值是列表长度：" + lpushx1);
+        //将一个值插入到列表头部（该方法不能插入一个不存在的List中,返回0），不能插入多个值只能插入一个值，不然会报错
+        Long lpushx1 = j.lpushx("mylist", "aa");
+        Long lpushx2 = j.lpushx("mylist1", "aa");
+        System.out.println("lpushx(key,value)的返回值是列表长度：" + lpushx1);
         //判断key是否存在
         Boolean exists = j.exists("mylist1");
         System.out.println("exists(key)判断key是否存在" + exists);
-        System.out.println("lpushx(key,values...)插入一个不存在的key：" + lpushx2);
+        System.out.println("lpushx(key,value)插入一个不存在的key：" + lpushx2);
 
         //查看插入后的List
         forList("mylist");
@@ -87,19 +88,24 @@ public class ListController {
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
+        //查看list
+        System.out.println("删除前的list列表：");
+        forList("mylist");
+
         //移除List中的指定值
         //count > 0 : 从表头开始向表尾搜索，移除与 VALUE 相等的元素，数量为 COUNT 。
         //count < 0 : 从表尾开始向表头搜索，移除与 VALUE 相等的元素，数量为 COUNT 的绝对值。
         //count = 0 : 移除表中所有与 VALUE 相等的值。
         //返回值为被移除元素的数量。 列表不存在时返回 0 。
-        Long lrem = j.lrem("key", -1, "2");
+        Long lrem = j.lrem("mylist", -1, "aa");
+        System.out.println("j.lrem(key,count,value)的返回值为：" + lrem);
 
         //查看list
         forList("mylist");
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
-        System.out.println("查看修改前的list");
+        System.out.println("查看修改前的list：");
         forList("mylist");
 
         //通过索引设置列表元素的值,成功返回OK，否则返回错误信息。
@@ -112,6 +118,7 @@ public class ListController {
 
         //裁剪List，命令执行成功时，返回 ok 。
         String ltrim = j.ltrim("mylist", 2, -3);
+        System.out.println("j.ltrim(key,start,end)的返回值是：" + ltrim);
         //查看裁剪后的List
         forList("mylist");
 
@@ -121,21 +128,28 @@ public class ListController {
         String rpop = j.rpop("mylist");
         System.out.println("rpop(key)集合key被移除的最后一个元素：" + rpop);
 
-        //查看裁剪后的List
+        //查看List
         forList("mylist");
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
-        //在列表中添加一个或多个值,作用与lpush类似，不过是用尾部插入List中
-        j.rpush("mylist","A","B","C","D");
+        //在列表中添加一个或多个值,作用与lpush类似，不过是从尾部插入List中
+        Long rpush = j.rpush("mylist", "A", "B", "C", "D");
 
-        //为已存在的列表添加一个或多个值,作用与lpushx类似，不过是用尾部插入List中
-        j.rpushx("mylist","11","22","33","44");
+        //为已存在的列表添加一个值,作用与lpushx类似，不过是用尾部插入List中
+        Long rpushx = j.rpushx("mylist", "11");
+
+        System.out.println("j.rpush(key,values...)的返回值为：" + rpush);
+        System.out.println("j.rpushx(key,value)的返回值为：" + rpushx);
+
+        //查看List
+        forList("mylist");
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
         //将列表中最后一个值弹出，存入另一个列表里，并返回该值
         String rpoplpush = j.rpoplpush("mylist", "mylist1");
+        System.out.println("j.rpoplpush(srckey,dstkey)弹出的值为：" + rpoplpush);
 
         //查看列表
         System.out.println("mylist:");
@@ -163,13 +177,46 @@ public class ListController {
 
         System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
 
-        //从列表中弹出一个值，将弹出的元素插入到另外一个列表中并返回它；
+        //从列表中弹出最后一个值，将弹出的元素插入到另外一个列表中并返回它；
         //如果列表没有元素会阻塞列表直到等待超时或发现可弹出元素为止。
         //假如在指定时间内没有任何元素被弹出，则返回一个 nil 和等待时长。
         //有值则返回一个含有两个元素的列表，第一个元素是被弹出元素的值，第二个元素是等待时长。
         String brpoplpush = j.brpoplpush("mylist", "mylist1", 100);
         System.out.println(brpoplpush);
 
+
+        return Result.success();
+    }
+
+    @GetMapping("/testlrem")
+    public ResponseMessage testlrem(){
+        Jedis j = JedisUtil.getInstance().getJedis();
+        Long lpush = j.lpush("testlrem", "a", "b", "c", "d", "a", "b", "c", "d", "a", "b", "c", "d");
+        System.out.println("---修改前的list：---");
+        System.out.println("--------lpush:" + lpush);
+        forList("testlrem");
+
+        System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+
+        Long lrem1 = j.lrem("testlrem", 0, "a");
+        System.out.println("---j.lrem(\"testlrem\",0,\"a\")---" + lrem1);
+        forList("testlrem");
+
+        System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+
+        Long lrem2 = j.lrem("testlrem", -2, "b");
+        System.out.println("---j.lrem(\"testlrem\",-2,\"b\")---" + lrem2);
+        forList("testlrem");
+
+        System.out.println("->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->");
+
+        Long lrem3 = j.lrem("testlrem", 1, "c");
+        System.out.println("---j.lrem(\"testlrem\",1,\"c\")---" + lrem3);
+        forList("testlrem");
+
+        Transaction multi = j.multi();
+
+        List<Object> exec = multi.exec();
 
         return Result.success();
     }
